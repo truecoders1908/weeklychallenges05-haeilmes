5d35991b8351296f11c56b0ecb44bf7d4c80f624
Hunter <haeilmes@gmail.com>
2019-09-27 13:33:09 -0500

    Got one test to pass

--- weeklyChallenges05/ChallengesSet05.cs
+++ weeklyChallenges05/ChallengesSet05.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 
@@ -13,12 +14,32 @@ namespace weeklyChallenges05
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            foreach(var business in businesses)
+            {
+                if(business.TotalRevenue == 0)
+                {
+                    business.Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers != null && numbers.Length > 1)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    while (numbers[i] <= numbers[i + 1])
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if (numbers.Length == 1)
+            {
+                return true;
+            }
+            return false;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
@@ -28,7 +49,12 @@ namespace weeklyChallenges05
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if(words is null || words.Length < 1)
+            {
+                return "";
+            }
+
+            return String.Join(" ", words.Where(word => !string.IsNullOrEmpty(word) || word == " ")) + ".";
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
