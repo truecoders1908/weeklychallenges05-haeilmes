518b861198dc8b86c571c4d9a4bc2276232b3582
Hunter <haeilmes@gmail.com>
2019-10-01 08:24:32 -0500

    Updated Challenges

--- weeklyChallenges05/ChallengesSet05.cs
+++ weeklyChallenges05/ChallengesSet05.cs
@@ -9,7 +9,13 @@ namespace weeklyChallenges05
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            int x = startNumber;
+            do
+            {
+                x++;
+            }
+            while (x % n != 0);
+            return x;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
@@ -25,50 +31,76 @@ namespace weeklyChallenges05
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            if (numbers != null && numbers.Length > 1)
-            {
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    while (numbers[i] <= numbers[i + 1])
-                    {
-                        return true;
-                    }
-                }
-            }
-            else if (numbers.Length == 1)
-            {
-                return true;
-            }
-            return false;
+            if (numbers != null && numbers.Length != 0)
+                return numbers.Zip(numbers.Skip(1), (curr, next) => curr <= next).All(x => x);
+            else
+                return false;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            int y = 0;
+            if (numbers == null)
+            {
+                return 0;
+            }
+            foreach (var item in numbers)
+            {
+                if (x > 0)
+                {
+                    y += item;
+                    x--;
+                }
+                if (item % 2 == 0)
+                    x++;
+            }
+            return y;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            if(words is null || words.Length < 1)
+            if (words is null || words.Length < 1)
             {
-                return "";
+                return " ";
             }
-
             return String.Join(" ", words.Where(word => !string.IsNullOrEmpty(word) || word == " ")) + ".";
         }
-
-        public double[] GetEveryFourthElement(List<double> elements)
+            public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            double[] low = new double[0];
+            List<double> fourth = new List<double>();
+            fourth.Add(4);
+            List<double> list2 = new List<double>();
+            int x = 0;
+            if (elements == null || elements.Count < 4)
+                return low;
+            else if (elements.Count == 4)
+                return fourth.ToArray();
+            else
+            {
+                foreach (double item in elements)
+                {
+                    x += 1;
+                    if (x == 4)
+                    {
+                        list2.Add(item);
+                        x -= 4;
+                    }
+                }
+            }
+            return list2.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            for (int i = 0; i < nums.Length; i++)
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
             {
-                if (nums[i] + nums[i] == targetNumber)
+                for (int j = nums.Length - 1; j > i; j--)
                 {
-                    return true;
+                    if (nums[i] + nums[j] == targetNumber)
+                        return true;
                 }
             }
             return false;
